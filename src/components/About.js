957f83dd62b4a157290d615ad3259ba5b3496f4c
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-04-12 13:38:46 +0300

    Make About and Links components use props

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,35 @@
-import React from "react";
+// import React from "react";
+// import Links from "./Links";
 
-function About() {
+// function About() {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       <p>Put the bio in here</p>
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       {/* add your <Links /> component here */}
+//       {bio ? <p>{bio}</p> : null}
+//       <Links github={links.github} linkedin={links.linkedin} />
+//     </div>
+//   );
+// }
+
+// export default About;
+
+import React from "react";
+import Links from "./Links";
+
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* only show the <p> if there's a bio */}
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
